f4ff37ee197d2151fec4869028be5ed349778a45
Cuphead74832 <119815517+Cuphead74832@users.noreply.github.com>
2025-11-21 15:52:01 +0100

    Update messenger-enable.js
    Tails1154 edited this part.

--- zefie_wtvp_minisrv/includes/ServiceVault/wtv-setup/messenger-enable.js
+++ zefie_wtvp_minisrv/includes/ServiceVault/wtv-setup/messenger-enable.js
@@ -1,21 +1,14 @@
 const minisrv_service_file = true;
 let justenabled, enablestatus;
 
-if (!session_data.getSessionData("messenger_enabled") === 1) {
-	session_data.setSessionData("messenger_enabled", 1);
-	session_data.saveSessionData;
-	justenabled = "true";
-	enablestatus = "1";
-} else {
-	session_data.setSessionData("messenger_enabled", 0);
-	session_data.saveSessionData;
-	justenabled = "false"
-	enablestatus = "0"
-}
-
-headers = `300 OK
-Content-type: text/html
-wtv-expire: wtv-setup:/messenger
+if (true) {                                                                                   session_data.setSessionData("messenger_enabled", 1);
+    session_data.saveSessionData;                                                             justenabled = "true";
+    enablestatus = "1";
+} else {                                                                                      session_data.setSessionData("messenger_enabled", 0);                                      session_data.saveSessionData;
+    justenabled = "false"
+    enablestatus = "0"
+}                                                                                         
+headers = `300 OK                                                                         Content-type: text/html                                                                   wtv-expire: wtv-setup:/messenger
 Location: wtv-setup:/messenger?just_enabled=${justenabled}
 wtv-messenger-enable: ${enablestatus}
 `;
